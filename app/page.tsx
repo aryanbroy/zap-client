2ff2882899b4e03623135a0cf4ff672aeec3d0d1
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-03-30 01:33:10 +0530

    add navbar to every page

--- app/page.tsx
+++ app/page.tsx
@@ -28,47 +28,6 @@ export default function LandingPage() {
   const router = useRouter();
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="px-8 lg:px-6 h-16 flex items-center">
-        <Link className="flex items-center justify-center" href="#">
-          <Zap className="h-6 w-6 text-primary" />
-          <span className="ml-2 text-xl font-bold">Zapp</span>
-        </Link>
-        <nav className="ml-auto flex gap-6 sm:gap-6">
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="#"
-          >
-            Features
-          </Link>
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="#"
-          >
-            Pricing
-          </Link>
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="#"
-          >
-            Integrations
-          </Link>
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="#"
-          >
-            Documentation
-          </Link>
-        </nav>
-        {/* <div className="ml-4 flex items-center gap-4">
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="#"
-          >
-            Log in
-          </Link>
-          <Button size="sm">Login</Button>
-        </div> */}
-      </header>
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 flex items-center justify-center">
           <div className="container px-4 md:px-6">
